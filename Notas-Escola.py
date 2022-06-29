c5057705ed0f34f2a18f6f0f26019fa438e3ee2d
Rodrigo Souza Silva <souza.rodrigo11@gmail.com>
2022-06-29 13:49:29 -0300

    Add files via upload

---
+++ Notas-Escola.py
@@ -0,0 +1,10 @@
+nota = int(input('Digite Um Numero :'))
+
+if nota > 90:
+     print('Nota A, E ESTA APROVADO.. PARABÉNS (:')
+if nota > 80 and nota <= 90:
+    print('Nota B')
+if nota >= 60 and nota <=80:
+    print('Nota C') 
+if nota >= 30 and nota <=60:
+    print('Nota E , E Esta Reprovado ! :( ')       
